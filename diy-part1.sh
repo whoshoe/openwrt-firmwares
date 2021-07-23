aeeb25df1545da6957ba2d78ec66276a6afd99b4
apeng0018 <80731248+apeng0018@users.noreply.github.com>
2021-07-24 06:09:16 +0800

    diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,6 +12,9 @@
 
 # Uncomment a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld'>>feeds.conf.default
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages'>>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small'>>feeds.conf.default
+echo 'src-git wiringPi https://github.com/nmaas87/rpi-openwrt-packages.git'>>feeds.conf.default
 ./scripts/feeds update -a && ./scripts/feeds install -a
 
 # Add a feed source
