e877c8d806e715c2abb33c9b83f0b5c7aa5c1fe7
apeng0018 <80731248+apeng0018@users.noreply.github.com>
2021-08-09 06:50:24 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,6 +14,7 @@
 echo 'src-git helloworld https://github.com/fw876/helloworld'>>feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages'>>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small'>>feeds.conf.default
+echo 'src-git SIM7600 https://github.com/aht-lab/ESP32_SIM7600'>>feeds.conf.default
 ./scripts/feeds update -a && ./scripts/feeds install -a
 
 # Add a feed source
