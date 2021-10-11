7a1af203cc4891c8dc82bfe43137b856bfa59deb
apeng0018 <80731248+apeng0018@users.noreply.github.com>
2021-10-12 00:06:47 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,8 +14,6 @@
 echo 'src-git helloworld https://github.com/fw876/helloworld'>>feeds.conf.default
 ./scripts/feeds update -a && ./scripts/feeds install -a
 
-#Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages'>>feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small'>>feeds.conf.default
-sed -i '$a src-git SIM7600 https://github.com/aht-lab/ESP32_SIM7600'>>feeds.conf.default
+Add a feed source
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
