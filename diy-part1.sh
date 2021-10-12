ac06c2c1721bf591cf8096377f35e088166dd6b4
apeng0018 <80731248+apeng0018@users.noreply.github.com>
2021-10-12 13:29:24 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,7 +10,7 @@
 #Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
- Uncomment a feed source
+# Uncomment a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld'>>feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages'>>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small'>>feeds.conf.default
