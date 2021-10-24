28a344a73d8eb0003b893a99a27ab82a8a835da0
apeng0018 <80731248+apeng0018@users.noreply.github.com>
2021-10-24 22:27:36 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,4 +1,4 @@
-!/bin/bash
+#!/bin/bash
 #
 # Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 #
@@ -6,15 +6,15 @@
 # See /LICENSE for more information.
 #
 # https://github.com/P3TERX/Actions-OpenWrt
- File name: diy-part1.sh
- Description: OpenWrt DIY script part 1 (Before Update feeds)
+# File name: diy-part1.sh
+#Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
- Uncomment a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld'>>feeds.conf.default
-./scripts/feeds update -a && ./scripts/feeds install -a
+# Uncomment a feed source
+#echo 'src-git helloworld https://github.com/fw876/helloworld'>>feeds.conf.default
+#./scripts/feeds update -a && ./scripts/feeds install -a
 
- Add a feed source
+# Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+#sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+#sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
