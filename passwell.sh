a7dba7b5787f02e1525b2e7f3e69928d68f0b23a
HT <huxu.htian@gmail.com>
2022-02-18 15:19:56 +0800

    Create passwell.sh

---
+++ passwell.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install upx -y
+cp /usr/bin/upx staging_dir/host/bin
+cp /usr/bin/upx-ucl staging_dir/host/bin
+
+./scripts/feeds update -a
+pushd feeds/packages/lang
+rm -rf golang && svn co https://github.com/openwrt/packages/branches/openwrt-21.02/lang/golang
+popd
+
+./scripts/feeds install luci-app-passwall
